4f3fcba5792b6398b4df8275d898f1066923145c
paxo-rch <paxo.rch@gmail.com>
2025-01-21 17:32:05 +0100

    gravity

--- main.py
+++ main.py
@@ -2,35 +2,38 @@ from ursina import *
 from ursina.shaders import lit_with_shadows_shader
 
 import random
+from math import *
 
 app = Ursina()
 
 points = []
 
-for i in range (100):
-    point = Entity(model='sphere', color=color.random_color(), scale=0.1, shader=lit_with_shadows_shader)
-    point.position = Vec3(random.uniform(-10, 10), random.uniform(-10, 10), random.uniform(-10, 10))
+class Star(Entity):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.velocity = Vec3(0, 0, 0)
+        
+    def update(self):
+        self.position += self.velocity
 
-EditorCamera()  # add camera controls for orbiting and moving the camera
+for i in range (100):
+    point = Star(model='sphere', color=color.random_color(), scale=0.1, shader=lit_with_shadows_shader)
+    point.position = Vec3(random.uniform(-10, 10), random.uniform(-10, 10), random.uniform(-1, 1))
+    points.append(point)
+
+def update():
+    global points
+    for i in range(len(points)):
+        for j in range(len(points)):
+            if(i != j):
+                points[i].velocity -= (points[i].position - points[j].position).normalized() * min((1 / (distance(points[i].position, points[j].position) ** 2)),0.01) * 0.001
+
+EditorCamera()
 
 class Player(Entity):
-
     def update(self):
         self.direction = Vec3(
-            self.forward * (held_keys['w'] - held_keys['s'])
-            + self.right * (held_keys['d'] - held_keys['a'])
-            ).normalized()  # get the direction we're trying to walk in.
+            self.forward * (held_keys['z'] - held_keys['s']) + self.right * (held_keys['d'] - held_keys['q']) + self.up * (held_keys['space'] - held_keys['shift'])
+        ).normalized()
 
-        origin = self.world_position + (self.up*.5) # the ray should start slightly up from the ground so we can walk up slopes or walk over small objects.
-        hit_info = raycast(origin , self.direction, ignore=(self,), distance=.5, debug=False)
-        if not hit_info.hit:
-            self.position += self.direction * 5 * time.dt
-        else:
-            print(hit_info.entity)
-
-Player(model='cube', origin_y=-.5, color=color.orange)
-wall_left = Entity(model='cube', collider='box', scale_y=3, origin_y=-.5, color=color.azure, x=-4)
-wall_right = duplicate(wall_left, x=4)
-camera.y = 2
 app.run()
-
