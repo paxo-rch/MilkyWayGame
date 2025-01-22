c42df63dcf3380547a2425f91e69361b60ad3200
paxo-rch <paxo.rch@gmail.com>
2025-01-22 12:33:18 +0100

    time shown

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from ursina import *
 from ursina.shaders import lit_with_shadows_shader
+from ursina.prefabs.slider import Slider
+
 import random
 from math import *
 
@@ -19,11 +21,9 @@ class BlackHole(Entity):
         self.update_rate = 1
     def update(self):
         global time_count, base_time
-        
-        
+        time_rate = 1 * time_speed
         time_count += time.dt*time_rate
-        
-        #print(time_count)
+        print(time_count)
 
 class Star(Entity):
     def __init__(self, **kwargs):
@@ -55,7 +55,7 @@ for i in range (numstars):
 
     rm = 5e20 # en m
     r = random.uniform(rm/20,rm)
-    v = sqrt(center.blackhole_gravity * 6.674e-11 / r)
+    v = sqrt(center.blackhole_gravity * 6.674e-11 / r) * 100000
     a = random.uniform(0, 2 * pi)
     
     point.angular_velocity = v / r
@@ -66,17 +66,51 @@ for i in range (numstars):
     point.position = Vec3(0, 1 / (1 + exp((r * scale)/50*3 - 3))*random.uniform(-20, 20), 0)
     
     points.append(point)
+    
 
-"""distances = []
 
-for i in range(len(points)):
-    for j in range(len(points)):
-        if i != j:
-            distances.append(distance(points[i].position, points[j].position))
-            
-print(max(distances))
-print(min(distances))
-print(sum(distances) / len(distances))"""
+
+def convert_seconds(seconds):
+    units = [
+        (1, "seconde"),
+        (60, "minute"),
+        (3600, "heure"),
+        (86400, "jour"),
+        (31536000, "année"),
+        (31536000000, "millénaire"),
+        (31536000000000, "million d'année"),
+        (31536000000000000, "milliard d'année")
+    ]
+    
+    result = []
+    for factor, unit in reversed(units):
+        value = seconds // factor
+        if value > 0:
+            result.append(f"{int(value)} {unit}{'s' if value > 1 else ''}")
+            seconds %= factor
+    
+    return ", ".join(result) if result else "0 seconde"
+
+thin_slider = ThinSlider(text='', dynamic=True)
+
+thin_slider.label.origin = (0,0)
+thin_slider.label.position = (.25, -.1)
+
+def up_logarithm():
+    global time_speed
+    thin_slider.label.text = str(10 ** (thin_slider.value*15))
+    time_speed = (10 ** (thin_slider.value*15))
+
+up_logarithm()
+thin_slider.on_value_changed = up_logarithm
+
+label = Text(text='Time: 0 seconde', position=(-.75, -.45), origin=(0,0), align='left')
+
+def update():
+    global time_count, base_time, label
+    
+    label.text = f'Time: {convert_seconds(time_count)}'
+
 EditorCamera()
 
 class Player(Entity):
